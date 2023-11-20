ebb2775c6e7c7e7491b3f3f410920b89e7b74a9e
vanessarabie <vanessarabie12.com>
2023-11-20 18:31:19 +0200

    you are done

---
+++ prac.js
@@ -0,0 +1,11 @@
+var myvar = "my name is vaan";
+console.log(myvar);
+
+var myvar = null;
+console.log(myvar);//null
+console.log(typeof myvar);//object
+
+var x = null;
+var y;
+console.log(x==y);
+console.log(x!=y);
