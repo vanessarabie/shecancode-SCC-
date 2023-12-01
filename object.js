41394255a4a9cba338e8d20661f745bf5d065361
vanessarabie <vanessarabie12.com>
2023-12-01 18:11:46 +0200

    assessment practice on

---
+++ object.js
@@ -0,0 +1,38 @@
+let testObj = {
+ nr: 600,
+ str: "text"
+ };
+ console.log(testObj.nr);
+ console.log(testObj.str);
+
+
+
+ let use1 = {
+    name : "vanessa",
+    age : 12,
+    sex : "female",
+ };
+
+ console.log(use1.name);
+
+
+
+ let week = ["mon","tue","wed","thur","fri"];
+ console.log(week[0]);
+
+
+ let user1 = [
+    {
+        names : "nessa",
+        country : "rwanda",
+    },
+
+   {
+        names : "rabie",
+        country : "kenya",
+    }
+ ]
+ console.log(user1[0],user1[1]);
+
+
+ 
